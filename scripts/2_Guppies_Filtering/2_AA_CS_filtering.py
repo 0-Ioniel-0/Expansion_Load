6c77a5d768ef29ea33c68727cb0a049113ee78b2
Katarzyna <62282523+0-Ioniel-0@users.noreply.github.com>
2024-12-03 11:59:50 +0100

    Update 2_AA_CS_filtering.py

--- scripts/2_Guppies_Filtering/2_AA_CS_filtering.py
+++ scripts/2_Guppies_Filtering/2_AA_CS_filtering.py
@@ -8,32 +8,32 @@ parser.add_argument('-o', '--out', help='Output vcf file.', type=str, required=T
 parser.add_argument('-b', '--bed', help='BED file with ancestral alleles in 4th column and conservation'
                                         'score in 5th (after chromosome, start and end.', type=str,
                     required=True)
-parser.add_argument('-f', '--fd', help='Sites info file.', type=str, required=False)
-parser.add_argument('-l', '--lines', help='Total lines in file (used for counting progress.', type=str, required=True)
+parser.add_argument('-f', '--fd', help='Sites info file.', type=str, required=True)
+parser.add_argument('-l', '--lines', help='Total lines in file (used for counting progress.', type=str,
+                    required=True)
 args = parser.parse_args()
 
 print("Hello!")
 print("We are running FILTERING Ancestral-Alleles/Conservation-Score script")
 print("Input File: ", args.vcf)
 print("Output File: ", args.out)
-if args.fd is not None:
-    print("Sites info file: ", args.fd)
+print("Sites info file: ", args.fd)
 print("Working...")
 
-# Here, we read the BED file and save it.
+
 def read_bed_file(bed_file):
     aa_bed_data = {}
     cs_bed_data = {}
     with open(bed_file, 'r') as bed:
         for line in bed:
             chrom, start, end, allele, cs = line.strip().split('\t')
-            aa_bed_data[(chrom, int(start))] = allele
-            cs_bed_data[(chrom, int(start))] = cs
+            aa_bed_data[(chrom, str(start))] = allele
+            cs_bed_data[(chrom, str(start))] = cs
     return aa_bed_data, cs_bed_data
 
-# Here we loop over variants and look for AA and CS for given variant.
-def update_vcf_with_bed_info(vcf_file, aa_bed_data, cs_bed_data, output_file):
-    with (open(vcf_file, 'r') as vcf, open(output_file, 'w') as out):
+
+def update_vcf_with_bed_info(vcf_file, aa_bed_data, cs_bed_data, output_file, fd_file):
+    with open(vcf_file, 'r') as vcf, open(output_file, 'w') as out:
         line_number = 1
         total_lines = int(args.lines)
         for line in vcf:
@@ -41,8 +41,6 @@ def update_vcf_with_bed_info(vcf_file, aa_bed_data, cs_bed_data, output_file):
             reason = "None."
             chrom = "0"
             pos = "0"
-            
-            # Adding lines to VCF header about added fields.
             if line.startswith('#'):
                 if line.startswith('##INFO=<ID=ReadPosRankSum'):
                     out.write(line)
@@ -54,11 +52,10 @@ def update_vcf_with_bed_info(vcf_file, aa_bed_data, cs_bed_data, output_file):
                     out.write(line)  # Write header lines directly to output file
             else:
                 fields = line.strip().split('\t')
-                chrom, pos, ref, alt = fields[0], int(fields[1]), fields[3], fields[4]
+                chrom, pos, ref, alt = fields[0], str(fields[1]), fields[3], fields[4]
                 info = fields[7]
-                if args.fd is not None:
-                    with open(args.fd, 'a') as x:
-                        print(chrom, pos, file=x, end="\t")
+                with open(fd_file, 'a') as fd:
+                    fd.write(chrom + pos + "\t")
                 print("Line", line_number, "of", total_lines, (line_number*100)/total_lines, "%")
 
                 if (chrom, pos) in aa_bed_data:
@@ -69,8 +66,8 @@ def update_vcf_with_bed_info(vcf_file, aa_bed_data, cs_bed_data, output_file):
                         samples = fields[9:]
                         if allele_from_bed == alt:
                             reason = "Swapped genotypes."
-                            new_alt = ref
-                            new_ref = alt
+                            new_alt = str(ref)
+                            new_ref = str(alt)
                             fields[3] = new_ref
                             fields[4] = new_alt
                             samples = fields[9:]
@@ -93,16 +90,14 @@ def update_vcf_with_bed_info(vcf_file, aa_bed_data, cs_bed_data, output_file):
                         reason = "No AA."
                 else:
                     reason = "No CS."
-            if args.fd is not None:
-                with open(args.fd, 'a') as x:
-                    print("Reason: ", reason, file=x, end="\n")
+            with open(fd_file, 'a') as fd:
+                fd.write("Reason: " + reason + "\n")
 
             line_number = line_number + 1
 
 
 AA_data = read_bed_file(args.bed)
 print("BED file loaded. Filtering sites...")
-update_vcf_with_bed_info(args.vcf, *AA_data, args.out)
+update_vcf_with_bed_info(args.vcf, *AA_data, output_file = args.out, fd_file = args.fd)
 
 print("Finished.")
-
